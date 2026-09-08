4bf878d2a12e6d5fa5883f0dd2816023fa326ca2
Athaya <risyad.athaya2007@gmail.com>
2026-09-08 14:01:23 +0700

    feat(experience): add new page and its test

--- main/views.py
+++ main/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from main.models import Experience
 
-# Create your views here.
+def show_main(request):
+    context = {
+        "name": "Risyad Athaya Muhammad",
+        "nickname": "Athaya",
+        "npm": "2506595890",
+        "study_program": "S1 Sistem Informasi",
+        "bio": (
+            "An undergraduate Information Systems student at the University of Indonesia. "
+            "Currently trying to learn software engineering, exploring opportunities, and actively pursuing knowledge."
+        ),
+    }
+    return render(request, "index.html", context)
+
+def show_experience(request):
+    context = {
+        "name": "Risyad Athaya Muhammad",
+        "nickname": "Athaya",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
