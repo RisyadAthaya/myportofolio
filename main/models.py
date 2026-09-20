4f42b7c61096fe789d4d4a7bb77d0458a1e215bf
Athaya <risyad.athaya2007@gmail.com>
2026-09-20 18:40:52 +0700

    feat(models): remove thumbnail field and set started_at default in experience model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import uuid
 from django.db import models
 
@@ -16,8 +17,7 @@ class Experience(models.Model):
     organization = models.CharField(max_length=255)
     description = models.TextField()
     category = models.CharField(max_length=20, choices=EXPERIENCE_CHOICES, default='full-time')
-    thumbnail = models.URLField(blank=True, null=True)
-    started_at = models.DateTimeField(auto_now_add=True)
+    started_at = models.DateTimeField(default=datetime.now())
     ended_at = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
